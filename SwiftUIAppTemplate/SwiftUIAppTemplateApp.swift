203d34ba822fe23b598cb087db99022afbec036f
kajitanikento <kajitanikento03@gmail.com>
2024-05-27 08:45:46 +0900

    add: add project

---
+++ SwiftUIAppTemplate/SwiftUIAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppTemplateApp.swift
+//  SwiftUIAppTemplate
+//
+//  Created by kajitani kento on 2024/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
